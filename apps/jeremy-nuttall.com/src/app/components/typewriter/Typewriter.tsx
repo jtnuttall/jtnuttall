2b191a02061251bf50e5c2625d8f02c7a67b9278
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-09-23 14:21:03 -0700

    refactor(typewriter): make typewriter single-file

--- apps/jeremy-nuttall.com/src/app/components/typewriter/Typewriter.tsx
+++ apps/jeremy-nuttall.com/src/app/components/typewriter/Typewriter.tsx
@@ -8,9 +8,38 @@ import {
 } from 'react';
 import { Typography, TypographyVariant } from '@mui/material';
 import useInterval from '../../hooks/useInterval';
-import { TypewriterAction, TypewriterActions } from './types';
 
-export { TypewriterActions, TypewriterAction };
+export enum TypewriterAction {
+  Write = 'typewriter/write',
+  Pause = 'typewriter/pause',
+  Delete = 'typewriter/delete',
+  Reset = 'typewriter/reset',
+}
+
+export type TypewriterActionData<T extends TypewriterAction> = {
+  type: T;
+};
+
+export type WriteAction = TypewriterActionData<TypewriterAction.Write> & {
+  text: string;
+  color?: string;
+};
+
+export type PauseAction = TypewriterActionData<TypewriterAction.Pause> & {
+  milliseconds: number;
+};
+
+export type DeleteAction = TypewriterActionData<TypewriterAction.Delete> & {
+  characters?: number;
+};
+
+export type ResetAction = TypewriterActionData<TypewriterAction.Reset>;
+
+export type TypewriterActions =
+  | WriteAction
+  | PauseAction
+  | DeleteAction
+  | ResetAction;
 
 const enum TypewriterInternalAction {
   WriteCharacter = 'typewriter/internal/write-char',
