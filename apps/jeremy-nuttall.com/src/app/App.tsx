a6fcac9f07f9608d6c4d2378643aa23f8a263c22
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-09-28 09:21:30 -0700

    feat(suspense): add skeleton suspense for app load

--- apps/jeremy-nuttall.com/src/app/App.tsx
+++ apps/jeremy-nuttall.com/src/app/App.tsx
@@ -1,26 +1,25 @@
-import { useState } from 'react';
+import { Suspense } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from '@jtnuttall/apollo-codegen';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './style/theme';
 import NavigationRoot from './containers/navigation/NavigationRoot';
+import LoadingPage from './pages/LoadingPage';
 
 const queryClient = new QueryClient();
 
-const App = (): JSX.Element => {
-  const [dark, setDark] = useState(true);
-
-  return (
+const App = (): JSX.Element => (
+  <Suspense fallback={<LoadingPage />}>
     <ApolloProvider client={apolloClient}>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={dark ? theme.dark : theme.light}>
+        <ThemeProvider theme={theme.dark}>
           <CssBaseline />
           <NavigationRoot />
         </ThemeProvider>
       </QueryClientProvider>
     </ApolloProvider>
-  );
-};
+  </Suspense>
+);
 
 export default App;
---
+++ apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@mui/material';
+
+const LoadingPage = (): JSX.Element => <Box>Loading...</Box>;
+
+export default LoadingPage;
