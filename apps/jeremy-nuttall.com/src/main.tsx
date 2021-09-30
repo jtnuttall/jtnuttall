bf6a6ba1540c97edffc9181fd46c58ba725d09fe
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-09-30 15:58:23 -0700

    fix(fonts): resolve issue where variable fonts didn't load at all for some browsers

--- apps/jeremy-nuttall.com/src/main.tsx
+++ apps/jeremy-nuttall.com/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import '@fontsource/space-grotesk/variable.css';
+import '@fontsource/space-grotesk/300.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/500.css';
+import '@fontsource/space-grotesk/700.css';
 import '@fontsource/space-mono';
 import App from './app/App';
 
