f1c9acdeca5eb7d95d111684038f5730d19e6d37
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-10-01 14:45:48 -0700

    feat(project-card/link): add functionality to project link cards

--- apps/jeremy-nuttall.com/src/app/components/projects/ProjectCardHeader.tsx
+++ apps/jeremy-nuttall.com/src/app/components/projects/ProjectCardHeader.tsx
@@ -1,10 +1,35 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React, { ReactNode, SyntheticEvent, useState } from 'react';
 import { ProjectItemFragment } from '@jtnuttall/apollo-codegen';
 import {
   MoreVert as MoreVertIcon,
   Link as LinkIcon,
 } from '@mui/icons-material';
-import { CardHeader, IconButton, Menu, MenuItem } from '@mui/material';
+import { CardHeader, IconButton, Link, Menu, MenuItem } from '@mui/material';
+
+type MenuLinkProps = {
+  href?: string;
+  onClick?: () => void;
+  children?: ReactNode;
+};
+
+const MenuLink = React.forwardRef<HTMLLIElement, MenuLinkProps>(
+  (props, ref) => {
+    const { href, onClick, children } = props;
+
+    return (
+      <Link
+        color="inherit"
+        underline="none"
+        onClick={onClick}
+        href={href}
+        target="_blank"
+        rel="noopener"
+      >
+        <MenuItem ref={ref}>{children}</MenuItem>
+      </Link>
+    );
+  },
+);
 
 type ProjectCardHeaderProps = {
   project: ProjectItemFragment;
@@ -25,7 +50,6 @@ const ProjectCardHeader = (props: ProjectCardHeaderProps): JSX.Element => {
   };
 
   const openUrl = (url?: string | null) => {
-    console.log(url);
     handleClose();
   };
 
@@ -49,14 +73,16 @@ const ProjectCardHeader = (props: ProjectCardHeaderProps): JSX.Element => {
         onClose={handleClose}
       >
         {project?.repositoryUrl && (
-          <MenuItem onClick={() => openUrl(project.repositoryUrl)}>
-            <LinkIcon sx={{ mr: 1 }} /> Repository
-          </MenuItem>
+          <MenuLink href={project.repositoryUrl} onClick={() => handleClose()}>
+            <LinkIcon sx={{ mr: 1 }} />
+            Repository
+          </MenuLink>
         )}
         {project?.exampleUrl && (
-          <MenuItem onClick={() => openUrl(project.exampleUrl)}>
-            <LinkIcon sx={{ mr: 1 }} /> Example
-          </MenuItem>
+          <MenuLink href={project.exampleUrl} onClick={() => handleClose()}>
+            <LinkIcon sx={{ mr: 1 }} />
+            Example
+          </MenuLink>
         )}
       </Menu>
     </>
