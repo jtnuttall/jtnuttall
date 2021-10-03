5703e50a4e991c4ef27177fd080eabe9da762866
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-10-03 05:57:13 -0700

    feat(loading-screen): rudimentary animations on loading screen

--- apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
+++ apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
@@ -1,45 +1,81 @@
-import { Box } from '@mui/material';
-import { animated, useSpring } from '@react-spring/web';
+import { useEffect, useState } from 'react';
+import { Box, styled } from '@mui/material';
+import { animated, useSpring, useTransition } from '@react-spring/web';
 
-const colors: [string, string, string][] = [
+const colors = [
   ['rgb(65, 88, 208)', 'rgb(208, 203, 65)', 'rgb(208, 65, 77)'],
   ['rgb(197, 99, 168)', 'rgb(99, 197, 158)', 'rgb(197, 163, 99)'],
   ['rgb(255, 204, 112)', 'rgb(255, 112, 214)', 'rgb(112, 255, 193)'],
-];
+] as const;
 
-type LoadingPageProps = {
-  color?: string;
-  backgroundColor?: string;
-};
+const FullScreenBase = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  min-height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FullScreen = animated(FullScreenBase);
 
 const LoadingPage = (): JSX.Element => {
-  const animatedStyles = useSpring({
-    loop: true,
-    from: {},
+  const [show, setShow] = useState(false);
+
+  const transitions = useTransition(show, {
+    from: {
+      scaleX: 0,
+      borderRadius: '100%',
+    },
+    enter: {
+      scaleX: 1,
+      borderRadius: '0%',
+    },
+    leave: {
+      scaleX: 0,
+      borderRadius: '100%',
+    },
+    reset: show,
+    reverse: show,
+    delay: 200,
   });
-  return (
-    <Box sx={{ width: '200px', mt: 10 }}>
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100" height="100" rx="20" />
-        <text
-          style={{
-            fontSize: '96px',
-            lineHeight: 1.25,
-            userSelect: 'none',
-          }}
-          x="20.40406"
-          y="81.456688"
-        >
-          <tspan
-            x="20.40406"
-            y="81.456688"
-            style={{ fontSize: '96px', fill: '#ffffff' }}
+
+  useEffect(() => {
+    setShow(true);
+    return () => setShow(false);
+  }, []);
+
+  return transitions(
+    (styles, item) =>
+      item && (
+        <FullScreen style={{ ...styles, backgroundColor: colors[2][0] }}>
+          <animated.svg
+            width="200px"
+            style={{ width: '200px', marginTop: 10, marginBottom: 10 }}
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            J
-          </tspan>
-        </text>
-      </svg>
-    </Box>
+            <rect width="100" height="100" rx="20" />
+            <text
+              style={{
+                fontSize: '96px',
+                lineHeight: 1.25,
+                userSelect: 'none',
+              }}
+              x="20.40406"
+              y="81.456688"
+            >
+              <tspan
+                x="20.40406"
+                y="81.456688"
+                style={{ fontSize: '96px', fill: '#ffffff' }}
+              >
+                J
+              </tspan>
+            </text>
+          </animated.svg>
+        </FullScreen>
+      ),
   );
 };
 
