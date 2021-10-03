cdf9a8c2b335e9bc22fb6e71189b1c948ad14638
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2021-10-03 06:58:13 -0700

    fix(loading-page): loading page no longer dependent on fonts and fills screen

--- apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
+++ apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box, styled } from '@mui/material';
+import { styled } from '@mui/material';
 import { animated, useSpring, useTransition } from '@react-spring/web';
 
 const colors = [
@@ -8,11 +8,10 @@ const colors = [
   ['rgb(255, 204, 112)', 'rgb(255, 112, 214)', 'rgb(112, 255, 193)'],
 ] as const;
 
-const FullScreenBase = styled(Box)`
+const FullScreenBase = styled('div')`
   display: flex;
   flex-direction: column;
-  flex: 1;
-  min-height: 100%;
+  height: 100vh;
   align-items: center;
   justify-content: center;
 `;
@@ -49,14 +48,17 @@ const LoadingPage = (): JSX.Element => {
     (styles, item) =>
       item && (
         <FullScreen style={{ ...styles, backgroundColor: colors[2][0] }}>
-          <animated.svg
+          <svg
             width="200px"
             style={{ width: '200px', marginTop: 10, marginBottom: 10 }}
             viewBox="0 0 100 100"
             xmlns="http://www.w3.org/2000/svg"
           >
             <rect width="100" height="100" rx="20" />
-            <text
+            <g aria-label="J" fill="#FFFFFF">
+              <path d="m 45.65206,82.800688 c 13.152,0 20.256,-7.488 20.256,-21.504 v -39.744 h 7.296 v -7.296 h -27.168 v 7.296 h 11.808 v 39.744 c 0,8.448 -3.36,14.112 -12.288,14.112 -8.832,0 -13.248,-5.664 -13.248,-14.112 v -5.184 h -7.968 v 5.184 c 0,13.92 8.16,21.504 21.312,21.504 z" />
+            </g>
+            {/* <text
               style={{
                 fontSize: '96px',
                 lineHeight: 1.25,
@@ -72,8 +74,8 @@ const LoadingPage = (): JSX.Element => {
               >
                 J
               </tspan>
-            </text>
-          </animated.svg>
+            </text> */}
+          </svg>
         </FullScreen>
       ),
   );
