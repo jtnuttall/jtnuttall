43e1a2e7487cac6bfd1672a4cdbbefc17e2bece9
jeremy nuttall <jeremy@jeremy-nuttall.com>
2022-01-20 17:57:32 -0800

    fix: use raw css for loading page

--- apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
+++ apps/jeremy-nuttall.com/src/app/pages/LoadingPage.tsx
@@ -5,25 +5,77 @@ const FullScreen = styled('div')`
   display: flex;
   flex-direction: column;
   height: 100vh;
-  width: 100vw;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
+`;
+
+const SimpleSpinner = styled('div')`
+  &,
+  &:before,
+  &:after {
+    background: #ffffff;
+    -webkit-animation: load1 1s infinite ease-in-out;
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+    height: 4em;
+  }
+  & {
+    color: #ffffff;
+    text-indent: -9999em;
+    margin: 88px auto;
+    position: relative;
+    font-size: 11px;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+  }
+  &:before,
+  &:after {
+    position: absolute;
+    top: 0;
+    content: '';
+  }
+  &:before {
+    left: -1.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+  &:after {
+    left: 1.5em;
+  }
+  @-webkit-keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+  @keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
 `;
 
 const LoadingPage = (): JSX.Element => {
   return (
-    <FullScreen style={{ backgroundColor: 'rgb(255, 204, 112)' }}>
-      <svg
-        width="200px"
-        style={{ width: '200px' }}
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect width="100" height="100" rx="20" fill="rgb(65, 88, 208)" />
-        <g aria-label="J" fill="#FFF">
-          <path d="m 45.65206,82.800688 c 13.152,0 20.256,-7.488 20.256,-21.504 v -39.744 h 7.296 v -7.296 h -27.168 v 7.296 h 11.808 v 39.744 c 0,8.448 -3.36,14.112 -12.288,14.112 -8.832,0 -13.248,-5.664 -13.248,-14.112 v -5.184 h -7.968 v 5.184 c 0,13.92 8.16,21.504 21.312,21.504 z" />
-        </g>
-      </svg>
+    <FullScreen style={{ backgroundColor: '#4d4a4a' }}>
+      <SimpleSpinner />
     </FullScreen>
   );
 };
