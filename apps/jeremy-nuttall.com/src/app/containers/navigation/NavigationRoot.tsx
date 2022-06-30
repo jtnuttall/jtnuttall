ab034b2eee7300fb408f4822842d9599b4c886e7
jeremy nuttall <jeremy@jeremy-nuttall.com>
2022-06-29 19:51:32 -0600

    feat: privacy-respecting analytics

--- apps/jeremy-nuttall.com/src/app/containers/navigation/NavigationRoot.tsx
+++ apps/jeremy-nuttall.com/src/app/containers/navigation/NavigationRoot.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { track } from 'insights-js';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import TopNavigationBar from '../../components/navigation/TopNavigationBar';
 import LoadingPage from '../../pages/LoadingPage';
 
 const HomePage = React.lazy(() => import('../../pages/HomePage'));
 const ProjectsPage = React.lazy(() => import('../../pages/ProjectsPage'));
 
-const NavigationRoot = (): JSX.Element => (
-  <BrowserRouter>
-    <TopNavigationBar />
+const TrackedRoutes = (): JSX.Element => {
+  const location = useLocation();
+
+  useEffect(() => {
+    track({
+      id: 'page-view',
+      parameters: {
+        path: location.pathname,
+      },
+    });
+  }, [location]);
+
+  return (
     <Routes>
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
+  );
+};
+
+const NavigationRoot = (): JSX.Element => (
+  <BrowserRouter>
+    <TopNavigationBar />
+    <TrackedRoutes />
   </BrowserRouter>
 );
 
