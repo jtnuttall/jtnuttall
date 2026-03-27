88bbee940bf4b5bf1e25f1cfde3900d8763e1573
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2026-03-26 20:40:05 -0500

    chore: add a basic flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Jeremy's portfolio entrypoint";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = builtins.attrValues {
+          inherit (pkgs) pnpm_10 nodejs_24 texliveFull;
+        };
+      };
+    });
+}
